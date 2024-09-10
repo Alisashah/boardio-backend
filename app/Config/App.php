347ea2a90de0cbfdf7905fbfcc1e484003a009f9
Alisashah <112863132+Alisashah@users.noreply.github.com>
2024-09-10 16:34:52 +0500

    update index

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://boardio-backend-aliz4wssp-boardio.vercel.app/';
+    public string $baseURL = 'https://boardio-backend.vercel.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
